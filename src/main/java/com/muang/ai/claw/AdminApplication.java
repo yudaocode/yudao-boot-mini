a29187beb2e592c3bcfceee1d0e27a1099220190
jyycool <451865995@qq.com>
2026-06-05 11:36:02 +0800

    移动文件

--- src/main/java/com/muang/ai/claw/AdminApplication.java
+++ src/main/java/com/muang/ai/claw/AdminApplication.java
@@ -1,4 +1,4 @@
-package com.muang.ai.claw.server;
+package com.muang.ai.claw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,15 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${yudao.info.base-package}
 @SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
-public class ServerApplication {
+public class AdminApplication {
 
     public static void main(String[] args) {
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
         // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
 
-        SpringApplication.run(ServerApplication.class, args);
-//        new SpringApplicationBuilder(ServerApplication.class)
+        SpringApplication.run(AdminApplication.class, args);
+//        new SpringApplicationBuilder(YudaoServerApplication.class)
 //                .applicationStartup(new BufferingApplicationStartup(20480))
 //                .run(args);
 
